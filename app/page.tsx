47750aa3de29cc50d103cf812366cece033df26e
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-01-06 22:30:14 +0300

    chore: fox typo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="min-h-screen bg-amber-50">
       <div className="flex flex-col items-center justify-center h-screen my-auto">
         <h1 className="mb-5 text-4xl font-bold">Home page 🏡</h1>
-        <h1 className="mb-5 text-xl">This page be visible to everyone ✅</h1>
+        <h1 className="mb-5 text-xl">This page should be visible to everyone ✅</h1>
         <Link
           href="/dashboard"
           className="p-3 bg-amber-200 block w-fit rounded-primary font-bold hover:bg-amber-500"
