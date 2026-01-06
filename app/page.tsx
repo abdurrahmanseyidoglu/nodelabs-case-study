0585b7ac626dfe4f97a403d7d5eea4e737c6ece4
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-01-06 20:36:23 +0300

    feat: enhance home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <h1 className="mb-5">Woah! Such empty :D</h1>
-      <Link
-        href="/dashboard"
-        className="p-3 bg-amber-200 block w-fit rounded-primary hover:bg-amber-500"
-      >
-        Go to Dashboard
-      </Link>
+    <div className="min-h-screen bg-amber-50">
+      <div className="flex flex-col items-center justify-center h-screen my-auto">
+        <h1 className="mb-5 text-4xl font-bold">Home page 🏡</h1>
+        <h1 className="mb-5 text-xl">This page be visible to everyone ✅</h1>
+        <Link
+          href="/dashboard"
+          className="p-3 bg-amber-200 block w-fit rounded-primary font-bold hover:bg-amber-500"
+        >
+          Go to Dashboard 🔐
+        </Link>
+      </div>
     </div>
   );
 }
