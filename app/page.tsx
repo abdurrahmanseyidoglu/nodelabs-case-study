54e325b3f6c20eb0f43dad348a8b33f977f35879
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-01-08 11:54:24 +0300

    feat: redirect to signin if user is not authenticated and to dashboard if he is

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-amber-50">
-      <div className="flex flex-col items-center justify-center h-screen my-auto">
-        <h1 className="mb-5 text-4xl font-bold">Home page 🏡</h1>
-        <h1 className="mb-5 text-xl">This page should be visible to everyone ✅</h1>
-        <Link
-          href="/dashboard"
-          className="p-3 bg-amber-200 block w-fit rounded-primary font-bold hover:bg-amber-500"
-        >
-          Go to Dashboard 🔐
-        </Link>
-      </div>
-    </div>
-  );
+export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/dashboard");
+  } else {
+    redirect("/signin");
+  }
 }
